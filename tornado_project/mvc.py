e2114ba939c986327733c27a4f58c0a287e0924b
Toni Haka-Risku <thakarisku@qvantel.com>
2017-06-10 22:21:48 +0300

    Tornado example created

--- tornado_project/mvc.py
+++ tornado_project/mvc.py
@@ -3,21 +3,38 @@ from tornado import websocket
 
 
 class Model(websocket.WebSocketHandler):
+    counter = 0
+
     def __init__(self, application, request, **kwargs):
         super().__init__(application, request, **kwargs)
-        self.counter = 0
 
     def check_origin(self, origin):
         return True
 
-    def on_message(self, message):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_close(self):
+        print("WebSocket closed")
+
+    def increase(self):
         self.counter += 1
-        self.write_message(u"State changed: " + str(self.counter))
+        self.write_message(str(self.counter))
+
+    def decrease(self):
+        self.counter -= 1
+
+    def on_message(self, message):
+        {"get": lambda: True,
+         "increase": self.increase,
+         "decrease": self.decrease}[message]()
+
+        self.write_message(str(self.counter))
 
 
 def make_app():
     return tornado.web.Application([
-        (r"/websocket", Model),
+        (r"/model", Model),
     ], debug=True)
 
 if __name__ == "__main__":
